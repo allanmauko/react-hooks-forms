92f6fbef843e2d4900d29e908cd6fd56dd470b7b
allanmauko <allan.mauko@student.moringaschool.com>
2022-07-22 13:33:26 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import React from "react";
+import ReactDOM from "react-dom";
+import ParentComponent from "./ParentComponent";
 
 ReactDOM.render(
-  <Form />,
+  <ParentComponent />,
   document.getElementById('root')
 );
